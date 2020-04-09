c9adc859f9fc5df7d6c9fad710c2d9ad16f8e573
NewLuminous <newluminous7@gmail.com>
2020-04-09 23:46:09 +0700

    Add script to remove trailing '\r' from files

--- scripts/remove-cr.sh
+++ scripts/remove-cr.sh
@@ -7,12 +7,6 @@ file=$1
 if [ "$1" ]; then
   sed -i 's/\r$//' $file
 else
-  sed -i 's/\r$//' scripts/add-crawled-data.sh
-  sed -i 's/\r$//' scripts/schema-api.sh
-  sed -i 's/\r$//' scripts/crawl.sh
-  sed -i 's/\r$//' scripts/create-cores.sh
-  sed -i 's/\r$//' scripts/start-solr.sh
   sed -i 's/\r$//' scripts/start-web.sh
-  sed -i 's/\r$//' scripts/test.sh
   sed -i 's/\r$//' scripts/wait.sh
 fi
