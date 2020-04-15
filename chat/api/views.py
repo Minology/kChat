4596afacc1aa45b7317c907f359711d52a4746ea
Minology <35617941+Minology@users.noreply.github.com>
2020-04-15 10:43:22 +0700

    add Message API

--- chat/api/views.py
+++ chat/api/views.py
@@ -7,9 +7,9 @@ from rest_framework.generics import (
     DestroyAPIView,
     UpdateAPIView
 )
-from chat.models import Conversation, Participant
-from chat.views import get_user
-from .serializers import ConversationSerializer
+from chat.models import Conversation, Participant, Message
+from chat.views import get_user, get_conversation
+from .serializers import ConversationSerializer, MessageSerializer
 
 User = get_user_model()
 
@@ -51,4 +51,41 @@ class ConversationUpdateView(UpdateAPIView):
 class ConversationDeleteView(DestroyAPIView):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+
+class MessageListView(ListAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = (permissions.AllowAny, )
+
+    def get_queryset(self):
+        queryset = Message.objects.all()
+        conversation_id = self.request.query_params.get('conv', None)
+        if conversation_id is not None:
+            conversation = get_conversation(conversation_id)
+            queryset = Message.objects.filter(conversation_id=conversation_id)
+        return queryset
+
+
+class MessageDetailView(RetrieveAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = (permissions.AllowAny, )
+
+
+class MessageCreateView(CreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+
+class MessageUpdateView(UpdateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+
+class MessageDeleteView(DestroyAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
     permission_classes = (permissions.IsAuthenticated, )
