ea021ed40ebcc1f920c3147bc1a7242a78707496
Minology <35617941+Minology@users.noreply.github.com>
2020-04-20 11:02:04 +0700

    Update searching and friendship related functions

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,7 +2,7 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 from django.contrib.auth import get_user_model
 from asgiref.sync import async_to_sync
-from .models import Message, Conversation, AttachmentType, Participant
+from .models import Message, Conversation, AttachmentType, Participant, Connection, FriendRequest
 from .views import get_last_10_messages, get_user, get_conversation, get_attachment_type
 
 User = get_user_model()
@@ -11,6 +11,8 @@ User = get_user_model()
 class ChatConsumer(WebsocketConsumer):
     # fetch last 10 messages of a group chat
     def fetch_messages(self, data):
+        mydata = {'username':'admin', 'conversation_id':1, 'friend_username':'root', 'from_username':'admin','to_username':'descendingblade'}
+        self.decline_friend_request(mydata)
         messages = get_last_10_messages(data['conversation_id'])
         content = {
             'command': 'messages',
@@ -181,6 +183,141 @@ class ChatConsumer(WebsocketConsumer):
         }
         self.send_data(content)  
 
+    def search_conversation(self, data):
+        conversations = Conversation.objects.filter(title__contains = data['text'])
+        content = {
+            'command': 'search_conversation',
+            'conversations': self.conversations_to_json(conversations)
+        }
+        self.send_data(content) 
+
+    def search_user(self, data):
+        users = User.objects.filter(username__contains = data['text'])
+        content = {
+            'command': 'search_user',
+            'users': self.users_to_json(users)
+        }
+        self.send_data(content)
+
+    def fetch_user_info(self, data):
+        user = get_user(data['username'])
+        content = {
+            'command': 'fetch_user_info',
+            'user': self.user_to_json(user)
+        }
+        self.send_data(content)
+
+    def fetch_all_users_info(self):
+        content = {
+            'command': 'fetch_all_users_info',
+            'users': self.users_to_json(User.objects.all())
+        }
+        self.send_data(content)
+
+    def fetch_not_friends(self, data):
+        user = get_user(data['username'])
+        connections = user.connections.all()
+        friends_id = [user.id]
+        for connection in connections:
+            friends_id.append(connection.to_user.id)
+        strangers = User.objects.exclude(id__in=friends_id)
+        content = {
+            'command': 'fetch_not_friends',
+            'strangers': self.users_to_json(strangers)
+        }
+        self.send_data(content)
+
+    def fetch_all_friends(self, data):
+        user = get_user(data['username'])
+        connections = user.connections.all()
+        friends = []
+        for connection in connections:
+            friends.append(connection.to_user)
+        content = {
+            'command': 'fetch_all_friends',
+            'friends': self.users_to_json(friends)
+        }
+        self.send_data(content)
+
+    def send_friend_request(self, data):
+        from_user = get_user(data['from_username'])
+        to_user = get_user(data['to_username'])
+        request, created = FriendRequest.objects.get_or_create(
+            from_user=from_user,
+            to_user=to_user
+        )
+        content = {
+            'command': 'send_friend_request',
+            'request': self.friend_request_to_json(request)
+        }
+        self.send_data(content)
+
+    def fetch_friend_requests_of_user(self, data):
+        to_user = get_user(data['to_username'])
+        requests = to_user.friend_requests.all()
+        content = {
+            'command': 'fetch_friend_requests_of_user',
+            'requests': self.friend_requests_to_json(requests)
+        }
+        self.send_data(content)
+
+    def accept_friend_request(self, data):
+        from_user = get_user(data['from_username'])
+        to_user = get_user(data['to_username'])
+        FriendRequest.objects.filter(from_user=from_user, to_user=to_user).delete()
+        connections = []
+        connections.append(Connection.objects.get_or_create(from_user=from_user, to_user=to_user)[0])
+        connections.append(Connection.objects.get_or_create(from_user=to_user, to_user=from_user)[0])
+        content = {
+            'command': 'accept_friend_request',
+            'connections': self.friend_requests_to_json(connections)
+        }
+        self.send_data(content)
+
+    def decline_friend_request(self, data):
+        from_user = get_user(data['from_username'])
+        to_user = get_user(data['to_username'])
+        FriendRequest.objects.filter(from_user=from_user, to_user=to_user).delete()
+        content = {
+            'command': 'decline_friend_request',
+            'log': 'Decline friend request successful',
+        }        
+        self.send_data(content)
+
+    def remove_friend(self, data):
+        from_user = get_user(data['username'])
+        to_user = get_user(data['friend_username'])
+        Connection.objects.filter(from_user=from_user, to_user=to_user).delete()
+        Connection.objects.filter(from_user=to_user, to_user=from_user).delete()
+        content = {
+            'command': 'remove_friend',
+            'log': 'Remove friend successful',
+        }
+        self.send_data(content)
+
+    def fetch_friends_outside_of_conversation(self, data):
+        conversation = get_conversation(data['conversation_id'])
+        participants = Participant.objects.filter(conversation=conversation)
+        participants_id = []
+        for participant in participants:
+            participants_id.append(participant.id)
+
+        user = get_user(data['username'])
+        connections = user.connections.all()
+        friends = []
+        for connection in connections:
+            friends.append(connection.to_user)
+        
+        friends_outside = []
+        for friend in friends:
+            if friend.id not in participants_id:
+                friends_outside.append(friend)
+
+        content = {
+            'command': 'friends_outside_of_conversation',
+            'friends_outside': self.users_to_json(friends_outside)
+        }
+        self.send_data(content)
 
     def users_to_json(self, users):
         result = []
@@ -194,7 +331,9 @@ class ChatConsumer(WebsocketConsumer):
             'username': user.username,            
             'first_name': user.first_name,
             'last_name': user.last_name,
-            'email': user.email,
+            'email': user.profile.email,
+            'quote': user.profile.quote,
+            'place': user.profile.place,
         } 
 
     def conversations_to_json(self, conversations):
@@ -214,6 +353,19 @@ class ChatConsumer(WebsocketConsumer):
             'deleted_at': str(conversation.deleted_at),
         } 
 
+    def friend_requests_to_json(self, requests):
+        result = []
+        for request in requests:
+            result.append(self.friend_request_to_json(request))
+        return result
+
+    def friend_request_to_json(self, request):
+        return {
+            'created_at': str(request.created_at),
+            'from_user': request.from_user.username,
+            'to_user': request.to_user.username,
+        }
+
     # Commands received from frontend
     commands = {
         'fetch_messages': fetch_messages,
@@ -224,5 +376,17 @@ class ChatConsumer(WebsocketConsumer):
         'add_user_to_conversation': add_user_to_conversation,
         'remove_user_from_conversation': remove_user_from_conversation,
         'create_conversation': create_conversation,
-        'remove_conversation': remove_conversation
+        'remove_conversation': remove_conversation,
+        'search_conversation': search_conversation,
+        'search_user': search_user,
+        'fetch_user_info': fetch_user_info,
+        'fetch_all_users_info': fetch_all_users_info,
+        'fetch_not_friends': fetch_not_friends,
+        'fetch_all_friends': fetch_all_friends,
+        'send_friend_request': send_friend_request,
+        'fetch_friend_requests_of_user': fetch_friend_requests_of_user,
+        'accept_friend_request': accept_friend_request,
+        'decline_friend_request': decline_friend_request,
+        'remove_friend': remove_friend,
+        'fetch_friends_outside_of_conversation': fetch_friends_outside_of_conversation,
     }
