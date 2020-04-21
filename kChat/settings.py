1e91877243cfca00b67648ae340257932d1cb312
Nguyễn Quốc An <quocanuetcs@gmail.com>
2020-04-21 16:03:18 +0700

    FIX
    [FIX] confirm email bug

--- kChat/settings.py
+++ kChat/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
-    'corsheaders',
 
     'logAPI',
     'rest_auth',
--- logAPI/urls.py
+++ logAPI/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
 from rest_auth.views import LoginView,LogoutView
-from rest_auth.registration.views import VerifyEmailView, RegisterView
-from allauth.account import views
+from rest_auth.registration.views import VerifyEmailView, RegisterView, ConfirmEmailView
 
 urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
-    url('^auth/registration/$', RegisterView.as_view(), name='register'),
-    url(r'^auth/verify-email/$', VerifyEmailView.as_view(), name='account_email_verification_sent'),
-    url(r'^auth/confirm-email/(?P<key>[-:\w]+)/$', views.confirm_email, name="account_confirm_email"),
+    url('^registration/$', RegisterView.as_view(), name='register'),
+    url(r'^verify-email/$', VerifyEmailView.as_view(), name='account_email_verification_sent'),
+    url(r'^confirm-email/(?P<key>[-:\w]+)/$', LoginView.as_view(), name="account_confirm_email"),
 ]
