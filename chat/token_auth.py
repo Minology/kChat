2c6ab6f94b84ec8bfc705b44ac2907dbab2ce68b
Minology <35617941+Minology@users.noreply.github.com>
2020-04-23 20:54:56 +0700

    Update authentication

--- chat/token_auth.py
+++ chat/token_auth.py
@@ -12,15 +12,18 @@ class TokenAuthMiddleware:
         self.inner = inner
 
     def __call__(self, scope):
-        headers = dict(scope['headers'])
-        if b'authorization' in headers:
+        headers = dict(scope["headers"])
+        if "X-Auth-Token" in headers[b"cookie"]:
             try:
-                token_name, token_key = headers[b'authorization'].decode().split()
-                if token_name == 'Token':
+                cookies = headers[b"cookie"].decode()
+                token_key = re.search("X-Auth-Token=(.*)(; )?", cookies).group(1)
+                if token_key:
                     token = Token.objects.get(key=token_key)
-                    scope['user'] = token.user
+                    scope["user"] = token.user
+                    close_old_connections()
             except Token.DoesNotExist:
-                scope['user'] = AnonymousUser()
+                scope["user"] = AnonymousUser()
+
         return self.inner(scope)
 
 TokenAuthMiddlewareStack = lambda inner: TokenAuthMiddleware(AuthMiddlewareStack(inner))
