7fc88fa09431afe7083897b449842bf013f5c83f
Minology <35617941+Minology@users.noreply.github.com>
2020-04-23 18:12:52 +0700

    Update rest framework configuration

--- kChat/settings.py
+++ kChat/settings.py
@@ -203,6 +203,9 @@ SOCIALACCOUNT_PROVIDERS = {
 
 # Rest Framework settings
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',  
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ],
