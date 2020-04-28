ae9cd2a6c3823049b8572d8f536f97222b809c12
Minology <35617941+Minology@users.noreply.github.com>
2020-04-28 11:22:57 +0700

    Update .travis.yml

--- kChat/settings.py
+++ kChat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -242,3 +243,5 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'chat.user'
 
 STATIC_ROOT = '/static/'
+django_heroku.settings(locals())
+
