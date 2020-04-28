17f656baad88578e09b21b88a08371b2e6fb8984
Minology <35617941+Minology@users.noreply.github.com>
2020-04-28 11:31:49 +0700

    Revert

--- kChat/settings.py
+++ kChat/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -243,5 +242,3 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'chat.user'
 
 STATIC_ROOT = '/static/'
-django_heroku.settings(locals())
-
